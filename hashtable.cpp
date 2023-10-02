c87c9dc700581d4b159739440f27e9dba71245ea
ll <tech@skl38.com>
2023-10-01 18:28:23 -0700

    wip

---
+++ hashtable.cpp
@@ -0,0 +1,16 @@
+#include "hashtable.h";
+
+HNode *hm_lookup(HMap *hmap, HNode *key, bool (*cmp)(HNode *, HNode)){
+
+}
+void hm_insert(HMap *hmap, HNode *node) {
+
+}
+
+HNode *hm_pop(HMap *hmap, HNode *key, bool (*cmp)(HNode *, HNode)){
+
+}
+
+void hm_destroy(HMap *hmap) {
+
+}
